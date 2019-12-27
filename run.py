a6c07b218b5721faea9b87524a6e42a4119a3528
Rahul Sharma <rahul.sharma@sourcefuse.com>
2019-12-27 19:39:40 +0530

    updated run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python
 import traceback
 
 import schedule
@@ -8,12 +8,14 @@ import subprocess
 import psutil
 import boto3
 import pickle
+import sys, base64, datetime, hashlib, hmac, urllib
+import requests # pip install requests
 
-from datetime import datetime
+#from datetime import datetime
 from operator import itemgetter
 
 
-rds = boto3.client('rds', region_name=os.getenv('REGION', 'us-east-1'))
+rds = boto3.client('rds', region_name=os.getenv('REGION', 'us-west-2'))
 
 log_state = dict()
 
@@ -21,72 +23,143 @@ log_state = dict()
 def raiser(e):
     raise e
 
-
 def download_log_files(db_name):
-    files = rds.describe_db_log_files(DBInstanceIdentifier=db_name)
+    #+++++++++++++++++++++++++++++++++++++++++
+    os.system('aws configure set aws_access_key_id "'+ os.getenv('AWS_ACCESS_KEY_ID') +'"')
+    os.system('aws configure set aws_secret_access_key "'+ os.getenv('AWS_SECRET_ACCESS_KEY') +'"')
+    os.system('aws configure set default.region "'+ os.getenv('REGION') +'"')
+    # ************* REQUEST VALUES *************
+    # method = 'GET'
+    # service = 'rds'
+    # host = 'rds.us-west-2.amazonaws.com'
+    # region = os.getenv('REGION')
+    # endpoint = 'https://' + host
+    # db_name = os.getenv('DB_NAME')
+    #===============================================
+    os.system('aws rds describe-db-log-files --db-instance-identifier "'+ db_name +'" > log.json')
+    os.system("grep 'LogFileName' log.json > lines.json")
+    os.system("cat lines.json | cut -d : -f 2 | cut -d / -f 2 | cut -d , -f1 | sed 's/\"//g' >lines")
+    text_file = open("lines", "r")
+    files = text_file.readlines()
+    print files
+    text_file.close()
+   # os.system("rm -rf lines")
+   # os.system("rm -rf log.json")
+   # os.system("rm -rf lines.json")
+    #=================================================
+    for file in files:
+        print file
+        method = 'GET'
+        service = 'rds'
+        region = os.getenv('REGION')
+        host = 'rds.' + region + '.amazonaws.com'
+        instance_name = os.getenv('DB_NAME')
+        logfile = 'error/'+file.strip()
+        rds_endpoint = 'https://' + host
+        uri = '/v13/downloadCompleteLogFile/' + instance_name + '/' + logfile
+        endpoint =  rds_endpoint + uri
 
-    # Sort based on touched time - ignore postgres.log
-    files = files.get('DescribeDBLogFiles')
-    files = [n.get('LogFileName') for n in
-             sorted(files, key=itemgetter('LastWritten'), reverse=True)
-             if n.get('LogFileName') != 'error/postgres.log']
+        # Key derivation functions. Taken from https://docs.aws.amazon.com/general/latest/gr/signature-v4-examples.html#signature-v4-examples-python
+        def sign(key, msg):
+            return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
 
-    # Get a weeks worth of files
-    files = files[:7]
+        def getSignatureKey(key, dateStamp, regionName, serviceName):
+            kDate = sign(('AWS4' + key).encode('utf-8'), dateStamp)
+            kRegion = sign(kDate, regionName)
+            kService = sign(kRegion, serviceName)
+            kSigning = sign(kService, 'aws4_request')
+            return kSigning
+        # Get session credentials
+        #session = session.Session()
 
-    # Download
-    for name in files:
-        print('Downloading {} ...'.format(name))
-        download_log(name, db_name)
+        #cred = session.get_credentials()
 
-    print('Downloads complete')
-    # Delete old files
-    for d in os.listdir('logs/'):
-        if os.path.isdir('./logs/' + d):
-            for f in os.listdir('logs/' + d):
-                if d + '/' + f not in files:
-                    os.remove('logs/' + d + '/' + f)
-        if os.path.isfile('logs/' + d):
-            if d not in files:
-                os.remove('logs/' + d)
+        #access_key = cred.access_key
 
+        #secret_key = cred.secret_key
+        access_key = os.getenv('AWS_ACCESS_KEY_ID')
+        secret_key = os.getenv('AWS_SECRET_ACCESS_KEY') 
+
+
+        if access_key is None or secret_key is None: 
+            print ("Credentials are not available.")
+            sys.exit()
+
+        # Create a date for headers and the credential string
+        t = datetime.datetime.utcnow()
+        amzdate = t.strftime('%Y%m%dT%H%M%SZ') # Format date as YYYYMMDD'T'HHMMSS'Z'
+        datestamp = t.strftime('%Y%m%d') # Date w/o time, used in credential scope
+
+        # Overview:
+        # Create a canonical request - https://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
+        # Sign the request.
+        # Attach headers.
+        # Send request
+
+        # Create canonical URI--the part of the URI from domain to query
+        canonical_uri = uri
+
+        # Create the canonical headers
+        canonical_headers = 'host:' + host + '\n' + 'x-amz-date:' + amzdate + '\n'
+        # signed_headers is the list of headers that are being included as part of the signing process.
+        signed_headers = 'host;x-amz-date'
+
+        # Using recommended hashing algorithm SHA-256
+        algorithm = 'AWS4-HMAC-SHA256'
+        credential_scope = datestamp + '/' + region + '/' + service + '/' + 'aws4_request'
+
+        # Canonical query string. All parameters are sent in http header instead in this example so leave this empty.
+        canonical_querystring = ''
+
+        # Create payload hash. For GET requests, the payload is an empty string ("").
+        payload_hash = hashlib.sha256(''.encode('utf-8')).hexdigest()
+
+        # Create create canonical request
+        canonical_request = method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n' + canonical_headers + '\n' + signed_headers + '\n' + payload_hash
+
+        # String to sign
+        string_to_sign = algorithm + '\n' +  amzdate + '\n' +  credential_scope + '\n' +  hashlib.sha256(canonical_request.encode('utf-8')).hexdigest()
+
+
+        # Create the signing key
+        signing_key = getSignatureKey(secret_key, datestamp, region, service)
+
+        # Sign the string_to_sign using the signing_key
+        signature = hmac.new(signing_key, (string_to_sign).encode("utf-8"), hashlib.sha256).hexdigest()
+
+        # Add signed info to the header
+        authorization_header = algorithm + ' ' + 'Credential=' + access_key + '/' + credential_scope + ', ' +  'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature
+        headers = {'Accept-Encoding':'gzip', 'x-amz-date':amzdate, 'Authorization':authorization_header}
+
+        # Send the request
+        r = requests.get(endpoint, headers=headers, stream=True)
+
+        print ("Logs Downloaded!")
+        print ("Response Code: " + str(r.status_code))
+        #print ("Content-Encoding: " + r.headers['content-encoding'])
+
+        oname = 'output2.txt'
+        with open(oname, 'a') as f:
+            for part in r.iter_content(chunk_size=8192):
+                f.write(str(part).replace(r'\n', '\n'))
+
+        print ("Log file saved to " + oname)
+
+    os.system("mv output2.txt logs/")
+    files=["output2.txt"]    
     return files
 
-
-def download_log(log_name, db_name):
-    marker = log_state.get(log_name, '0')
-    mode = 'ab'
-    if marker == '0':
-        mode = 'wb'
-    with open('logs/' + log_name, mode) as f:
-        try:
-            while True:
-                result = rds.download_db_log_file_portion(
-                    DBInstanceIdentifier=db_name,
-                    Marker=marker,
-                    LogFileName=log_name,
-                    NumberOfLines=99999999
-                )
-                # For debugging
-                # if not result.get('AdditionalDataPending'):
-                #     print('no data pending, marker:{}'
-                #           .format(result.get('Marker')))
-                if marker == result.get('Marker'):  # no more data
-                    marker = result.get('Marker')
-                    return
-                f.write(result.get('LogFileData').encode())
-                marker = result.get('Marker')
-        finally:
-            log_state[log_name] = marker
-
-
 def run_pgbadger(file_list):
+    START_TIME = os.getenv('START_TIME') or \
+              raiser(ValueError('START_TIME is required'))
+    END_TIME = os.getenv('END_TIME') or \
+              raiser(ValueError('END_TIME is required'))
     file_list = ['logs/' + f for f in file_list]
     subprocess.check_call([
         './pgbadger',
         '-j', str(psutil.cpu_count()),
-        '-X',
-        '-I',
+        '-b', START_TIME,
+        '-e', END_TIME,
         '-O', os.getcwd() + '/pg_reports',
         '-p', '%t:%r:%u@%d:[%p]:'
         ] + file_list
@@ -121,7 +194,7 @@ def get_log_states():
     try:
         with open('logs/status.p', 'rb') as p:
             log_state = pickle.load(p)
-    except FileNotFoundError:
+    except Exception:
         log_state = dict()
 
 
@@ -152,7 +225,7 @@ def run():
 
 def build_schedule():
     print('Starting sqlcron. Current time: {}'
-          .format(str(datetime.now())))
+          .format(str(datetime.datetime.now())))
     interval = int(os.getenv('INTERVAL', '1'))
     unit = os.getenv('UNIT', 'day')
     time_of_day = os.getenv('TIME')
@@ -167,7 +240,7 @@ def build_schedule():
 
 def run_schedule():
     while True:
-        sleep_time = schedule.next_run() - datetime.now()
+        sleep_time = schedule.next_run() - datetime.datetime.now()
         print('Next job to run at {}, which is {} from now'
               .format(str(schedule.next_run()), str(sleep_time)))
 
